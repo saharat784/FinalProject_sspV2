9f375a2279e69a623c577d5a608ce2316ed80da2
unknown <saharat.wi.66@ubu.ac.th>
2026-01-17 17:51:11 +0700

    newupdate cloudinary

--- smart_study_planner/settings.py
+++ smart_study_planner/settings.py
@@ -60,7 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # ✅ เพิ่มบรรทัดนี้ (ต้องอยู่ต่อจาก SecurityMiddleware ทันที)
+     # ✅ เพิ่มบรรทัดนี้ (ต้องอยู่ต่อจาก SecurityMiddleware ทันที)
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
